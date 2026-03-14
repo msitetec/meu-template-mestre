ef321145ec65494715340112ff571a4f2c4102fb
msitetec <msitetec@gmail.com>
2026-03-14 13:15:13 -0300

    🚀 Template Mestre: Setup Inicial Concluído

---
+++ meu-template-mestre/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-slate-50 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-indigo-600">Aguardando Código do Canvas ✨</h1>
+    </div>
+  );
+};
+
+export default App;
